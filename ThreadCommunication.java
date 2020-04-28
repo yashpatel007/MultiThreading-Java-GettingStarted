4ed2f7c41682b451254d74a5fd8b049ba0fa2f35
unknown <patelyash1311@gmail.com>
2020-04-28 18:54:45 -0400

    thread comminication

--- ThreadCommunication.java
+++ ThreadCommunication.java
@@ -1,1 +1,83 @@
 // here we are taking the classic example of producer consumer problem
+
+
+class ThreadCommunication{
+
+	public static void main(String[] args){
+		Q q = new Q();
+		new Producer(q);
+		new Consumer(q);
+
+	}
+
+}
+
+
+class Q{
+
+	int num;
+	Boolean valueSet =false;
+	public synchronized void set(int num){
+
+		while(valueSet){
+			try {wait();} catch(Exception e){}
+		}
+
+		System.out.println("set : "+num);
+		this.num=num;
+		valueSet = true;
+		notify();
+	}
+	public synchronized void get(){
+		while(!valueSet){
+			try {wait();} catch(Exception e){}
+		}
+		System.out.println("get : "+num);
+		valueSet=false;
+		notify();
+	}
+
+}
+
+class Producer implements Runnable{
+
+	Q q;
+
+	public Producer(Q q){
+		
+		this.q=q;
+
+		Thread t = new Thread(this,"producer");
+		t.start();
+	}
+
+	public void run(){
+	int i=0;
+	while(true){
+		q.set(i++);
+		try{ Thread.sleep(1000);}catch(Exception e){}
+	}
+
+	}
+
+}
+
+class Consumer implements Runnable{
+
+	Q q;
+	public Consumer(Q q){
+		
+		this.q=q;
+			Thread t = new Thread(this,"consumer");
+		t.start();
+	}
+
+	public void run(){
+	while(true){
+		q.get();
+		try{ Thread.sleep(1000);}catch(Exception e){}
+	}
+
+	}
+
+}
